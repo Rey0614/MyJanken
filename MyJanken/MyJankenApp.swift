bb7b668d2d2605e4e5d79665c81da47d233e5c9e
Rey <95080700+Rey0614@users.noreply.github.com>
2021-11-27 00:09:48 +0900

    first commit
    練習

---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by Rei Hashimoto on 2021/11/26.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
